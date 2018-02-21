525067d4a898ce974082b4c4d6a52c8ffb4eb507
mvietba <m.vietba@gmail.com>
2018-02-21 14:16:40 +0200

    Initial commit

---
+++ Assets/WorldController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldController : MonoBehaviour {
+	public Text txt;
+	// Use this for initialization
+	void Start () {
+		txt.text = "Hi! This is a visual acuity test!\n" +
+			"To start off, please stand at the red bar marked on the floor.\n" +
+			"Press trigger once you're done.";
+	}
+	
+	// Update is called once per frame
+	//1 unit = 1 meter
+	void Update () {
+		
+	}
+}
